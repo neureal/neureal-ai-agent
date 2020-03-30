45d54e12b2913d6291849b338dff6e3efc4f6ea1
Wil Bown <wilbown@gmail.com>
2020-03-29 18:34:32 -0600

    set tabs to 4 spaces

--- test-CVAE.py
+++ test-CVAE.py
@@ -1,8 +1,11 @@
-import tensorflow as tf
 import numpy as np
-import time
+# np.set_printoptions(precision=8, suppress=True)
+import tensorflow as tf
 import matplotlib.pyplot as plt
+import time
 import PIL
+
+# https://www.tensorflow.org/tutorials/generative/cvae
 
 (train_images, _), (test_images, _) = tf.keras.datasets.mnist.load_data()
 
@@ -28,94 +31,87 @@ train_dataset = tf.data.Dataset.from_tensor_slices(train_images).shuffle(TRAIN_B
 test_dataset = tf.data.Dataset.from_tensor_slices(test_images).shuffle(TEST_BUF).batch(BATCH_SIZE)
 
 class CVAE(tf.keras.Model):
-  def __init__(self, latent_dim):
-    super(CVAE, self).__init__()
-    self.latent_dim = latent_dim
-    self.representer_net = tf.keras.Sequential(
-      [
-          tf.keras.layers.InputLayer(input_shape=(28, 28, 1)),
-          tf.keras.layers.Conv2D(
-              filters=32, kernel_size=3, strides=(2, 2), activation='relu'),
-          tf.keras.layers.Conv2D(
-              filters=64, kernel_size=3, strides=(2, 2), activation='relu'),
-          tf.keras.layers.Flatten(),
-          # No activation
-          tf.keras.layers.Dense(latent_dim + latent_dim),
-      ]
-    )
+    def __init__(self, latent_dim):
+        super(CVAE, self).__init__()
+        self.latent_dim = latent_dim
+        self.representer_net = tf.keras.Sequential([
+            tf.keras.layers.InputLayer(input_shape=(28, 28, 1)),
+            tf.keras.layers.Conv2D(filters=32, kernel_size=3, strides=(2, 2), activation='relu'),
+            tf.keras.layers.Conv2D(filters=64, kernel_size=3, strides=(2, 2), activation='relu'),
+            tf.keras.layers.Flatten(),
+            # No activation
+            tf.keras.layers.Dense(latent_dim + latent_dim),
+        ])
 
-    self.generator_net = tf.keras.Sequential(
-        [
-          tf.keras.layers.InputLayer(input_shape=(latent_dim,)),
-          tf.keras.layers.Dense(units=7*7*32, activation=tf.nn.relu),
-          tf.keras.layers.Reshape(target_shape=(7, 7, 32)),
-          tf.keras.layers.Conv2DTranspose(
-              filters=64,
-              kernel_size=3,
-              strides=(2, 2),
-              padding="SAME",
-              activation='relu'),
-          tf.keras.layers.Conv2DTranspose(
-              filters=32,
-              kernel_size=3,
-              strides=(2, 2),
-              padding="SAME",
-              activation='relu'),
-          # No activation
-          tf.keras.layers.Conv2DTranspose(
-              filters=1,
-              kernel_size=3,
-              strides=(1, 1),
-              padding="SAME"),
-        ]
-    )
+        self.generator_net = tf.keras.Sequential([
+            tf.keras.layers.InputLayer(input_shape=(latent_dim,)),
+            tf.keras.layers.Dense(units=7*7*32, activation=tf.nn.relu),
+            tf.keras.layers.Reshape(target_shape=(7, 7, 32)),
+            tf.keras.layers.Conv2DTranspose(
+                filters=64,
+                kernel_size=3,
+                strides=(2, 2),
+                padding="SAME",
+                activation='relu'),
+            tf.keras.layers.Conv2DTranspose(
+                filters=32,
+                kernel_size=3,
+                strides=(2, 2),
+                padding="SAME",
+                activation='relu'),
+            # No activation
+            tf.keras.layers.Conv2DTranspose(
+                filters=1,
+                kernel_size=3,
+                strides=(1, 1),
+                padding="SAME"),
+        ])
 
-  @tf.function
-  def sample(self, eps=None):
-    if eps is None:
-      eps = tf.random.normal(shape=(100, self.latent_dim))
-    return self.decode(eps, apply_sigmoid=True)
+    @tf.function
+    def sample(self, eps=None):
+        if eps is None:
+            eps = tf.random.normal(shape=(100, self.latent_dim))
+        return self.decode(eps, apply_sigmoid=True)
 
-  def encode(self, x):
-    mean, logvar = tf.split(self.representer_net(x), num_or_size_splits=2, axis=1)
-    return mean, logvar
+    def encode(self, x):
+        mean, logvar = tf.split(self.representer_net(x), num_or_size_splits=2, axis=1)
+        return mean, logvar
 
-  def reparameterize(self, mean, logvar):
-    eps = tf.random.normal(shape=mean.shape)
-    return eps * tf.exp(logvar * 0.5) + mean
+    def reparameterize(self, mean, logvar):
+        eps = tf.random.normal(shape=mean.shape)
+        return eps * tf.exp(logvar * 0.5) + mean
 
-  def decode(self, z, apply_sigmoid=False):
-    logits = self.generator_net(z) # input latent, output logits
-    if apply_sigmoid:
-      probs = tf.sigmoid(logits) # this gets the image as 0.0-1.0 from logits? why called probs? sigmoid: y = 1 / (1 + exp(-x))
-      return probs
-    return logits
+    def decode(self, z, apply_sigmoid=False):
+        logits = self.generator_net(z) # input latent, output logits
+        if apply_sigmoid:
+            probs = tf.sigmoid(logits) # this gets the image as 0.0-1.0 from logits? why called probs? sigmoid: y = 1 / (1 + exp(-x))
+            return probs
+        return logits
 
 optimizer = tf.keras.optimizers.Adam(1e-4)
 
 def log_normal_pdf(sample, mean, logvar, raxis=1):
-  log2pi = tf.math.log(2.0 * np.pi)
-  return tf.reduce_sum(-0.5 * ((sample - mean) ** 2.0 * tf.exp(-logvar) + logvar + log2pi),
-      axis=raxis)
+    log2pi = tf.math.log(2.0 * np.pi)
+    return tf.reduce_sum(-0.5 * ((sample - mean) ** 2.0 * tf.exp(-logvar) + logvar + log2pi), axis=raxis)
 
 @tf.function
 def compute_loss(model, x):
-  mean, logvar = model.encode(x)
-  z = model.reparameterize(mean, logvar)
-  x_logit = model.decode(z)
+    mean, logvar = model.encode(x)
+    z = model.reparameterize(mean, logvar)
+    x_logit = model.decode(z)
 
-  cross_ent = tf.nn.sigmoid_cross_entropy_with_logits(logits=x_logit, labels=x)
-  logpx_z = -tf.reduce_sum(cross_ent, axis=[1, 2, 3])
-  logpz = log_normal_pdf(z, 0.0, 0.0)
-  logqz_x = log_normal_pdf(z, mean, logvar)
-  return -tf.reduce_mean(logpx_z + logpz - logqz_x)
+    cross_ent = tf.nn.sigmoid_cross_entropy_with_logits(logits=x_logit, labels=x)
+    logpx_z = -tf.reduce_sum(cross_ent, axis=[1, 2, 3])
+    logpz = log_normal_pdf(z, 0.0, 0.0)
+    logqz_x = log_normal_pdf(z, mean, logvar)
+    return -tf.reduce_mean(logpx_z + logpz - logqz_x)
 
 @tf.function
 def compute_apply_gradients(model, x, optimizer):
-  with tf.GradientTape() as tape:
-    loss = compute_loss(model, x)
-  gradients = tape.gradient(loss, model.trainable_variables)
-  optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+    with tf.GradientTape() as tape:
+        loss = compute_loss(model, x)
+    gradients = tape.gradient(loss, model.trainable_variables)
+    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
 
 epochs = 100
 latent_dim = 50
@@ -126,37 +122,36 @@ random_vector_for_generation = tf.random.normal(shape=[num_examples_to_generate,
 model = CVAE(latent_dim)
 
 def generate_and_save_images(model, epoch, test_input):
-  predictions = model.sample(test_input)
-  fig = plt.figure(figsize=(4,4))
+    predictions = model.sample(test_input)
+    fig = plt.figure(figsize=(4,4))
 
-  for i in range(predictions.shape[0]):
-      plt.subplot(4, 4, i+1)
-      plt.imshow(predictions[i, :, :, 0], cmap='gray')
-      plt.axis('off')
+    for i in range(predictions.shape[0]):
+        plt.subplot(4, 4, i+1)
+        plt.imshow(predictions[i, :, :, 0], cmap='gray')
+        plt.axis('off')
 
-  # tight_layout minimizes the overlap between 2 sub-plots
-  plt.savefig('render/image_at_epoch_{:04d}.png'.format(epoch))
-  plt.show()
+    # tight_layout minimizes the overlap between 2 sub-plots
+    plt.savefig('render/image_at_epoch_{:04d}.png'.format(epoch))
+    plt.show()
 
 generate_and_save_images(model, 0, random_vector_for_generation)
 
 for epoch in range(1, epochs + 1):
-  start_time = time.time()
-  for train_x in train_dataset:
-    compute_apply_gradients(model, train_x, optimizer)
-  end_time = time.time()
+    start_time = time.time()
+    for train_x in train_dataset:
+        compute_apply_gradients(model, train_x, optimizer)
+    end_time = time.time()
 
-  if epoch % 1 == 0:
-    loss = tf.keras.metrics.Mean()
-    for test_x in test_dataset:
-      loss(compute_loss(model, test_x))
-    elbo = -loss.result()
-    print('Epoch: {}, Test set ELBO: {}, time elapse for current epoch {}'.format(epoch, elbo, end_time - start_time))
-    generate_and_save_images(model, epoch, random_vector_for_generation)
+    if epoch % 1 == 0:
+        loss = tf.keras.metrics.Mean()
+        for test_x in test_dataset:
+            loss(compute_loss(model, test_x))
+        elbo = -loss.result()
+        print('Epoch: {}, Test set ELBO: {}, time elapse for current epoch {}'.format(epoch, elbo, end_time - start_time))
+        generate_and_save_images(model, epoch, random_vector_for_generation)
 
 def display_image(epoch_no):
-  return PIL.Image.open('render/image_at_epoch_{:04d}.png'.format(epoch_no))
+    return PIL.Image.open('render/image_at_epoch_{:04d}.png'.format(epoch_no))
 
 plt.imshow(display_image(epochs))
-plt.axis('off')# Display images
-
+plt.axis('off') # Display images
