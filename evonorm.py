690711b893c7e7680620646078b7c5e258f327db
Wil Bown <wilbown@gmail.com>
2020-10-25 12:52:11 -0600

    working with continuous dist, better setup

--- evonorm.py
+++ evonorm.py
@@ -14,6 +14,16 @@ import matplotlib.pyplot as plt
 physical_devices_gpu = tf.config.list_physical_devices('GPU')
 for i in range(len(physical_devices_gpu)): tf.config.experimental.set_memory_growth(physical_devices_gpu[i], True)
 
+@tf.function
+def fixinfnan(t):
+    zero = tf.constant(0.0, dtype=tf.float64)
+    isinf = tf.math.is_inf(t)
+    isneg = tf.math.equal(tf.math.sign(t),-1.0)
+    ispos = tf.math.logical_not(isneg)
+    isninf, ispinf = tf.math.logical_and(isinf, isneg), tf.math.logical_and(isinf, ispos)
+    t = tf.where(ispinf, zero, t) # inf = 0.0
+    t = tf.where(tf.math.logical_or(tf.math.is_nan(t), isninf), tf.float64.min, t) # nan = tf.float32.min, -inf = tf.float32.min
+    return t
 
 # DEFAULT_EPSILON_VALUE = 1e-5
 # def instance_std(x, eps=DEFAULT_EPSILON_VALUE):
@@ -104,18 +114,8 @@ class EvoNormS0(tf.keras.layers.Layer):
 
 if __name__ == "__main__":
     (train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.cifar10.load_data()
-    # data_train, data_test = tf.keras.datasets.cifar10.load_data()
     # (train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.mnist.load_data()
     # train_images, train_labels, test_images, test_labels = np.expand_dims(train_images,-1), np.expand_dims(train_labels,-1), np.expand_dims(test_images,-1), np.expand_dims(test_labels,-1)
-
-    # test = np.transpose(data_train[0])
-    # test2 = np.transpose(data_train[1])
-    # test3 = np.stack((test,test2), axis=0)
-    # data_train = np.transpose(np.stack(data_train, axis=0))
-    # data_test = np.transpose(np.stack(data_test, axis=0))
-    # data_train = np.concatenate(data_train, data_test)
-    # np.random.shuffle(data_train)
-
     train_images, train_labels, test_images, test_labels = np.float64(train_images), np.int32(train_labels), np.float64(test_images), np.int32(test_labels)
     # train_images, test_images = train_images / 255.0, test_images / 255.0
 
@@ -139,7 +139,13 @@ if __name__ == "__main__":
     class MyModel(tf.keras.Model):
         def __init__(self):
             super(MyModel, self).__init__()
-            # use_bias does not affect the output
+            # use_bias does not affect the output of EvoNormS0
+            self.categories = 10
+
+            num_components, event_shape = 12, 1
+            params_size = self.categories # Categorical
+            params_size = tfp.layers.MixtureSameFamily.params_size(num_components,
+                component_params_size=tfp.layers.MultivariateNormalTriL.params_size(event_shape))
             
             # self.layer_conv2d_in = tf.keras.layers.Conv2D(filters=16, kernel_size=(3, 3), activation='relu')
             # # self.layer_conv2d_in = tf.keras.layers.Conv2D(filters=96, kernel_size=(3, 3), strides=(1, 1), activation='relu')
@@ -167,8 +173,8 @@ if __name__ == "__main__":
             # self.layer_conv2d_11 = tf.keras.layers.Conv2D(filters=192, kernel_size=(1, 1), strides=(1, 1), activation='relu')
             self.layer_conv2d_11 = tf.keras.layers.Conv2D(filters=192, kernel_size=(1, 1), strides=(1, 1), use_bias=False)
             self.layer_conv2d_11_evo = EvoNormS0(16)
-            self.layer_conv2d_12 = tf.keras.layers.Conv2D(filters=10, kernel_size=(1, 1), strides=(1, 1), use_bias=False)
-            self.layer_conv2d_12_evo = EvoNormS0(2)
+            self.layer_conv2d_12 = tf.keras.layers.Conv2D(filters=params_size, kernel_size=(1, 1), strides=(1, 1), use_bias=False)
+            # self.layer_conv2d_12_evo = EvoNormS0(int(params_size/5))
 
 
             # self.layer_flatten = tf.keras.layers.Flatten()
@@ -190,7 +196,9 @@ if __name__ == "__main__":
 
             # self.layer_dropout = tf.keras.layers.Dropout(0.2)
             self.layer_globalavg_logits_out = tf.keras.layers.GlobalAveragePooling2D()
-            # self.layer_dense_logits_out = tf.keras.layers.Dense(10, use_bias=False)
+            # self.layer_dense_logits_out = tf.keras.layers.Dense(params_size, use_bias=False)
+            
+            self.dist = tfp.layers.MixtureSameFamily(num_components, tfp.layers.MultivariateNormalTriL(event_shape))
 
         @tf.function
         def call(self, inputs, training=None):
@@ -211,7 +219,7 @@ if __name__ == "__main__":
             out = self.layer_conv2d_11(out)
             out = self.layer_conv2d_11_evo(out)
             out = self.layer_conv2d_12(out)
-            out = self.layer_conv2d_12_evo(out)
+            # out = self.layer_conv2d_12_evo(out)
             # out = self.layer_flatten(out)
             # out = self.layer_dense_01(out)
             # out = self.layer_dense_01_evo(out)
@@ -227,45 +235,54 @@ if __name__ == "__main__":
             # out = self.layer_dense_logits_out(out)
             return out
 
-        # _loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+        # _loss_scc = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
         def _loss(self, targets, outputs):
-            dist = tfp.distributions.Categorical(logits=outputs)
-            loss = dist.log_prob(targets)
-            return -tf.reduce_mean(loss)
+            # dist = tfp.distributions.Categorical(logits=outputs)
+            # loss = dist.log_prob(tf.squeeze(targets, axis=-1)) # Categorical
+            dist = self.dist(outputs)
+            loss = dist.log_prob(tf.cast(targets, dtype=tf.float64))
+            loss = fixinfnan(loss)
 
+            # loss = -tf.math.reduce_mean(loss)
+            # loss = tf.math.abs(loss)
+            loss = tf.math.negative(loss)
+
+            onehot = tf.one_hot(tf.squeeze(targets, axis=-1), self.categories)
+            softmax = tf.transpose(tf.map_fn(fn=lambda v:dist.prob(v), elems=tf.range(self.categories, dtype=tf.float64)))
+
+            return loss, onehot, tf.clip_by_value(softmax, 0.0, 1.0)
+
+        _optimizer = tf.keras.optimizers.Adam(learning_rate=1e-4)
+        # _optimizer = tf.keras.optimizers.SGD(learning_rate=1e-5)
         metric_train_loss = tf.keras.metrics.Mean()
-        # _optimizer = tf.keras.optimizers.Adam(learning_rate=1e-4)
-        _optimizer = tf.keras.optimizers.SGD(learning_rate=1e-5)
         @tf.function
         def train(self, inputs, targets):
-            targets = tf.squeeze(targets, axis=-1) # only needed for Categorical?
             with tf.GradientTape() as tape:
                 outputs = self(inputs, training=True)
-                loss = self._loss(targets, outputs)
+                loss = self._loss(targets, outputs)[0]
             gradients = tape.gradient(loss, self.trainable_variables)
             self._optimizer.apply_gradients(zip(gradients, self.trainable_variables))
-            self.metric_train_loss.update_state(loss)
+
+            self.metric_train_loss.update_state(tf.math.reduce_mean(loss))
             return outputs
         
-        metric_test_loss = tf.keras.metrics.Mean()
-        metric_test_acc = tf.keras.metrics.CategoricalAccuracy()
-        metric_test_auc = tf.keras.metrics.AUC()
+        metric_test_loss, metric_test_acc, metric_test_auc = tf.keras.metrics.Mean(), tf.keras.metrics.CategoricalAccuracy(), tf.keras.metrics.AUC()
         @tf.function
         def test(self, inputs, targets):
-            targets = tf.squeeze(targets, axis=-1) # only needed for Categorical?
             outputs = self(inputs, training=False)
-            self.metric_test_loss.update_state(self._loss(targets, outputs))
-            onehot, softmax = tf.one_hot(targets, outputs.shape[1]), tf.nn.softmax(outputs)
+            loss, onehot, softmax = self._loss(targets, outputs)
+
+            self.metric_test_loss.update_state(tf.math.reduce_mean(loss))
             self.metric_test_acc.update_state(onehot, softmax)
             self.metric_test_auc.update_state(onehot, softmax)
             return outputs
+
     model = MyModel()
     
     epocs = 10
-    batch_size = 1
+    batch_size = 10
     metric_train_loss, metric_test_loss, metric_test_acc, metric_test_auc = [], [], [], []
     for epoc in range(epocs):
-        # np.random.shuffle(train_images)
 
         model.metric_train_loss.reset_states()
         for i in range(0, train_images.shape[0], batch_size):
