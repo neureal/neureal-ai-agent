00ea6024a7149d9dc86f0864a1aefb658df5fc40
Wil Bown <wilbown@gmail.com>
2021-03-30 12:34:09 -0600

    Update async_wrapper.py

--- envs_local/async_wrapper.py
+++ envs_local/async_wrapper.py
@@ -1,3 +1,5 @@
+import time
+import multiprocessing as mp
 import numpy as np
 np.set_printoptions(precision=8, suppress=True, linewidth=400, threshold=100)
 # np.random.seed(0)
@@ -14,13 +16,21 @@ class AsyncWrapperEnv(gym.Env):
         self.metadata = env.metadata
         self.reward_range = env.reward_range
         self.spec = env.spec
-    def seed(self):
-        return self.env.seed()
+
+        # TODO setup process so agent can start using reset()
+        # self.proc = mp.Process(target=self._proc_run, name='ENV_PROC', args=(lock_print, process_ctrl, action, obs, reward))
+        # process_ctrl.value = 0
+        # self.proc.join()
+
+    def _proc_run(self):
+
+    def seed(self): return self.env.seed()
+    def close(self): return self.env.close()
+    def render(self, mode='human', close=False): return self.env.render(mode, close)
     def reset(self):
+        # TODO start new process and step env with env.action_noop (stop old process loop)
+        # self.proc.start()
         return self.env.reset()
     def step(self, action):
+        # TODO insert this action into noop stream
         return self.env.step(action)
-    def render(self, mode='human', close=False):
-        return self.env.render(mode, close)
-    def close(self):
-        return self.env.close()
